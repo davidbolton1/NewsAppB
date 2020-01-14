2b0d95ac114c14ed2e18c7725709c50b51eadf29
David Bolton <smdeox@live.com>
2020-01-14 18:04:56 -0500

    Added logout button functionality

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,17 @@ router.get('/', (req,res) => {
   })
 })
 
-
+router.get('/logout', (req, res, next) => {
+  if(req.session) {
+    req.session.destroy((error) => {
+      if(error) {
+        next(error)
+      }else {
+        res.redirect('/login')
+      }
+    })
+  }
+})
 
 router.get('/login', (req, res) => {
   res.render('login')
